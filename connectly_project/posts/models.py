d4cdacbc920181bbe7b1d063ab126ccab2838f77
El Jean L. Rementas <rementasej2002@gmail.com>
2026-02-08 03:42:19 -0500

    Enhancing the Connectly API with Validation and Relationships

--- connectly_project/posts/models.py
+++ connectly_project/posts/models.py
@@ -14,4 +14,13 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.content[:50]
+        return f"Post by {self.author.username}"
+
+class Comment(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on post {self.post.id}"
